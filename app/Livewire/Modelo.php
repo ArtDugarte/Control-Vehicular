65fe4ed769482d9ce55b0b557265b98dd0dcce31
Arthuro Dugarte <60824496+ArtDugarte@users.noreply.github.com>
2024-02-20 18:03:26 -0400

    Se listan modelos en su respectiva vista

--- app/Livewire/Modelo.php
+++ app/Livewire/Modelo.php
@@ -3,11 +3,25 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Modelo as MModelo;
+use App\Models\Marca as MMarca;
 
 class Modelo extends Component
 {
+
+    public $feedback = '';
+    public $marcas = [];
+
     public function render()
     {
-        return view('livewire.modelo');
+        $modelos = MModelo::all();
+        $marcas = MMarca::all();
+    
+        foreach ($modelos as $modelo) {
+            $marca = $marcas->where('id', $modelo->marca_id)->first();
+            $modelo -> nombre_marca = $marca ? $marca->nombre : 'Sin marca';
+        }
+
+        return view('livewire.modelo', compact('modelos', 'marcas'));
     }
 }
--- resources/views/livewire/modelo.blade.php
+++ resources/views/livewire/modelo.blade.php
@@ -1,3 +1,22 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <h3>Listado de Modelos</h3>
+
+    <div>
+        {{ $feedback }}
+    </div>
+
+    <ul>
+        @forelse ($modelos as $modelo)
+            <li>{{ $modelo->nombre_marca }}  {{ $modelo->nombre }}</li>
+        @empty
+            <p>No hay modelos registradas</p>
+        @endforelse
+    </ul>
+
+    <form method="POST">
+        <label for="">Nombre</label>
+        <input type="text" wire:model.live="nombre">
+        <button type="button" wire:click="store()">Guardar</button>
+        <button type="button" wire:click="resetForm()">Limpiar</button>
+    </form>
 </div>
